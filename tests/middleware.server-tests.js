8e2b30635cc9c12e2cd0049c9fbeff37ba854eb8
Julien Lecomte <jlecomte@yahoo-inc.com>
2013-08-21 16:23:57 -0700

    Added code coverage checks and increased coverage beyond 80%

--- tests/middleware.server-tests.js
+++ tests/middleware.server-tests.js
@@ -6,47 +6,66 @@ YUI.add('yahoo.middleware.mojito-jscheck-tests', function (Y, NAME) {
 
     var A = YUITest.Assert,
         suite = new YUITest.TestSuite(NAME),
-        mid,
+
+        midPath = require('path').join(__dirname, '../middleware/mojito-jscheck.js'),
+
+        appConfig = {
+            jscheck: {
+                cookie: {
+                    domain: '.yahoo.com'
+                }
+            }
+        },
 
         midConfig = {
             Y: Y,
             store: {
-                _appConfigStatic: {
-                    jscheck: {
-                        cookie: {
-                            domain: '.yahoo.com'
-                        }
-                    }
-                }
+                _appConfigStatic: appConfig
             }
-        };
+        },
+
+        mid;
 
     suite.add(new YUITest.TestCase({
 
         name: 'unit tests',
 
-        setUp: function () {
-            var midPath = require('path').join(__dirname, '../middleware/mojito-jscheck.js');
+        'the middleware should just pass through if jscheck is disabled': function () {
+            appConfig.jscheck.enabled = false;
             mid = require(midPath)(midConfig);
+
+            var req = {},
+                res = {},
+                next = function () {
+                    A.pass();
+                };
+
+            mid(req, res, next);
         },
 
         'js=0 is in the url, so the middleware should set the cookie': function () {
+            appConfig.jscheck.enabled = true;
+            mid = require(midPath)(midConfig);
+
             var headers = {},
 
                 req = {
-                    url: '/search?p=whatever&js=0'
+                    url: '/search?p=whatever&js=0',
+                    cookies: {}
                 },
 
                 res = {
                     setHeader: function (key, val) {
                         headers[key.toLowerCase()] = val;
                     }
+                },
+
+                next = function () {
+                    A.areSame(headers.hasOwnProperty('set-cookie'), true);
+                    A.areSame(0, headers['set-cookie'].indexOf('js=0;domain=.yahoo.com;expires='));
                 };
 
-            mid(req, res, function () {
-                A.areSame(headers.hasOwnProperty('set-cookie'), true);
-                A.areSame(0, headers['set-cookie'].indexOf('js=0;domain=.yahoo.com;expires='));
-            });
+            mid(req, res, next);
         }
     }));
 
